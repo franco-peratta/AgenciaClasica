47cb994d193ffe89b17a2d70b3455e953742c477
Franco Peratta <franco.peratta@hotmail.com>
2019-01-22 13:18:05 -0300

    GitHub pages deployment
    1st try

--- angular-todo/src/environments/environment.prod.ts
+++ angular-todo/src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase : {
+    apiKey: "AIzaSyA5fpXUUAEYMXtSDtay0tQcPoz6w6d6Cw8",
+    authDomain: "agenciaclasica.firebaseapp.com",
+    databaseURL: "https://agenciaclasica.firebaseio.com",
+    projectId: "agenciaclasica",
+    storageBucket: "agenciaclasica.appspot.com",
+    messagingSenderId: "948867894720"
+  }
 };
