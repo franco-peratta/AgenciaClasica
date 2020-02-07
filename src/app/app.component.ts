3d058b8b3ac3e58c302df065b24142fc82c4cd02
Franco Peratta <fperatta@pasante05.sis.cooperativaobrera.coop>
2020-02-07 12:56:24 -0300

    Agrego alerta cuando un user esta logueado
    Modifico el tamaño del navbar logo.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
+import { AuthService } from './services/auth.service';
 
 @Component({
   selector: 'app-root',
@@ -11,14 +12,25 @@ import { Title } from '@angular/platform-browser';
 export class AppComponent implements OnInit {
 
   title = 'Agencia Clásica&Moderna';
+  logged: boolean = false;
 
 
-  constructor(private titleService: Title) {
+  constructor(private titleService: Title, private auth: AuthService) {
 
   }
 
   ngOnInit(): void {
     this.titleService.setTitle(this.title);
+
+    this.auth.getUser().subscribe( asd => {
+      if(asd!==null){
+        this.logged=true;
+      }
+      else{
+        this.logged = false;
+      }
+    });
+
   }
 
 
