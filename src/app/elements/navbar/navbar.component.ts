8950bd0573836fd2fcda65bbffe482fd5335ddee
Franco Peratta <fperatta@pasante05.sis.cooperativaobrera.coop>
2020-02-11 13:02:23 -0300

    Arreglo footer y navbar, falta hacer arreglos
    El footer no muestra bien los iconos. Ver como instalar font-awesome.
    El navbar deberia marcar el nav activo basandome en el url.

--- src/app/elements/navbar/navbar.component.ts
+++ src/app/elements/navbar/navbar.component.ts
@@ -7,11 +7,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  collapse:boolean;
+  collapse: boolean;
 
   constructor() { }
 
   ngOnInit() {
     this.collapse = true;
+    // Seteo Home como nav activo
+    document.getElementById('nav1').classList.add("active");
+  }
+
+  change_active_nav(event: any) {
+    let id = event.currentTarget.attributes.id.nodeValue;
+
+    let elems = document.getElementsByClassName("nav-item");
+
+    // Quito la clase active de todos los elementos del navbar
+    for (let i = 0; i < elems.length; i++) {
+      elems[i].classList.remove("active");
+    }
+
+    // Agrego clase activa al elemento clickeado
+
+    if (id === 'nav0') {
+      // Si clickie el nav-logo marco como activo el nav Home
+      document.getElementById('nav1').classList.add("active");
+    }
+    else {
+      document.getElementById(id).classList.add("active");
+    }
+    this.collapse = !this.collapse;
   }
 }
