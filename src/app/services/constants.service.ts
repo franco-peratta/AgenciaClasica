eb8cb65183e2f0e5f5460c615e1b6832d1c6fc63
Franco Peratta <fperatta@pasante05.sis.cooperativaobrera.coop>
2020-02-14 11:15:28 -0300

    Agrego servicio de constantes globales para el futuro

---
+++ src/app/services/constants.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+// La creo por si le tengo que dar algun uso alguna vez
+export class ConstantsService {
+
+  readonly randomConstant: string = "";
+
+}
